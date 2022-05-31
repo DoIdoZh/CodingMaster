5bbc860d8a8c3b68d1b4db70fe58c9e74274f11e
doido <3063676257@qq.com>
2022-05-31 13:47:28 +0800

    14话

--- 14/add_sub.asm
+++ 14/add_sub.asm
@@ -2,6 +2,7 @@
 mov ax, 0x0001
 mov bx, 0x0002
 add ax, bx
+
 ;产生进位
 mov ax, 0xf000
 mov bx, 0x1000
@@ -11,11 +12,13 @@ add ax, bx
 mov cx, 0x0003
 mov dx, 0x0001
 sub cx, dx
-;减法
+
+;产生借位 减法
 mov cx, 0x0001
 mov dx, 0x0002
 sub cx, dx
 
 sleep: jmp sleep
+
 times 510 - ($ - $$) db 0
 db 0x55, 0xaa
