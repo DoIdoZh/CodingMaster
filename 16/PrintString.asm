5f654123d05b06ad189867847f9d3d78c487fe39
doido <3063676257@qq.com>
2022-05-31 19:53:25 +0800

    16话

---
+++ 16/PrintString.asm
@@ -0,0 +1,42 @@
+section code align=16 vstart=0x7c00
+
+    mov si, SayHello
+    xor di, di
+    call PrintString
+
+    mov si, SayByeBye
+    call PrintString
+    jmp END
+
+PrintString:
+    .setup:
+    mov ax, 0xb800
+    mov es, ax
+
+    mov bh, 0x07
+    mov cx, 0xffff
+
+    .printchar:
+    mov bl, [ds:si]
+    inc si
+    mov [es:di], bl
+    inc di
+    mov [es:di], bh
+    inc di
+    or bl, 0x00
+    jz .return
+    loop .printchar
+    .return:
+    ret
+
+SayHello db 'Hello!'
+         db 0x00
+SayByeBye db 'ByeBye!'
+          db 0x00
+
+END: jmp END
+
+;循环即补0
+jmp $
+times 510-($-$$) db 0
+db 0x55, 0xaa
