615bad54200024c4cd6d69e845f8d963314ed196
doido <3063676257@qq.com>
2022-06-01 21:40:58 +0800

    16话

--- 16/PrintString.asm
+++ 16/PrintString.asm
@@ -1,37 +1,37 @@
 section code align=16 vstart=0x7c00
 
-    mov si, SayHello
-    xor di, di
-    call PrintString
+    mov si, SayHello ; 将SayHello这段字符串的开始地址给si
+    xor di, di ; 将di清零
+    call PrintString ; 调用PrintString函数
 
-    mov si, SayByeBye
-    call PrintString
-    jmp END
+    mov si, SayByeBye ; 将SayByeBye这段字符串的开始地址给si
+    call PrintString ; 调用PrintString函数
+    jmp END ; 执行完后，跳转到最后
 
-PrintString:
-    .setup:
-    mov ax, 0xb800
-    mov es, ax
+PrintString: ; 标号
+    .setup: ; 伪指令，用来标号
+    mov ax, 0xb800 ; 0xb800文本模式下显存起始地址
+    mov es, ax ; 初始化段地址
 
-    mov bh, 0x07
-    mov cx, 0xffff
+    mov bh, 0x0c ; 初始化字符属性
+    mov cx, 0xffff ; 设置循环次数
 
-    .printchar:
-    mov bl, [ds:si]
-    inc si
-    mov [es:di], bl
-    inc di
-    mov [es:di], bh
-    inc di
-    or bl, 0x00
-    jz .return
-    loop .printchar
-    .return:
-    ret
+    .printchar: ; 伪指令，用来标号
+    mov bl, [ds:si] ; 从内存中取字符，复制到bl寄存器
+    inc si ; 偏移地址自增
+    mov [es:di], bl ; 将bl寄存器中的字符值复制到显存地址中
+    inc di ; 显存地址自增
+    mov [es:di], bh ; 字符属性复制到显存地址中
+    inc di ; 显存地址自增
+    or bl, 0x00 ; 将0与bl或起来
+    jz .return ; 零标志位为零条状
+    loop .printchar ; 循环指令
+    .return: ; 伪指令，用来标号
+    ret ; return
 
-END: jmp END
+END: jmp END ; 死循环
 
-SayHello db 'Hello!'
+SayHello db 'Hello!' ; 字节填写
          db 0x00
 SayByeBye db 'ByeBye!'
           db 0x00
